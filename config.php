3d2c4be83856417e43bea89a98eae8026c1a7fa2
dvarga <dvarga@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-10-06 10:58:21 +0000

    https Easyling endpoint
    git-svn-id: https://plugins.svn.wordpress.org/easyling-for-wp/tags/0.9.18@1002499 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- config.php
+++ config.php
@@ -7,5 +7,5 @@
 
 define ('PRODUCT_NAME', "Easyling");
 define ('PRODUCT_LOGO_URL', "http://storage.googleapis.com/easyling-whitelabel-logos/Easyling_logo_new-black.png");
-define ('TRANSLATION_PROXY_ENDPOINT', "http://app.easyling.com/_el/ext/");
+define ('TRANSLATION_PROXY_ENDPOINT', "https://app.easyling.com/_el/ext/");
 define ('WHITELABELED', false);
